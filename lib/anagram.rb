b4613830e79e3b0eb43a7f9a3bc9b420110057d8
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-04 05:17:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,10 @@ class Anagram
   def match(array)
     anagrams = []
     array.each do |word_2| 
-      if anagram.split("").sort == self.word
-        matches.push(anagram)
+      if word_2.split("").sort == self.word
+        anagrams.push(word_2)
       end
     end
-    matches 
+    anagrams 
   end
 end
